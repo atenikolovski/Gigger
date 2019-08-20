5055e58df34ac10950b71d44eefb9b878d666e51
Atanas Nikolovski <atanas.nikolovski@students.finki.ukim.mk>
2019-08-20 14:38:08 +0200

    Form for creating gigs added

---
+++ Gigger/Controllers/GigsController.cs
@@ -0,0 +1,27 @@
+﻿using Gigger.Models;
+using Gigger.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Gigger.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+        // GET: Gigs
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var ViewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            
+            return View(ViewModel);
+        }
+    }
+}
