18e6dedd8d1b3bf6fc86f7327eaf60f1ff9bdaed
Atanas Nikolovski <atanas.nikolovski@students.finki.ukim.mk>
2019-08-20 12:43:51 +0200

    InitialModel created

---
+++ Gigger/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Gigger.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Gigger.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Gigger.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
