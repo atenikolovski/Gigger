08a1145420855b84333c00bf16e0d1c983352172
Atanas Nikolovski <atanas.nikolovski@students.finki.ukim.mk>
2019-08-21 10:44:40 +0200

    Implement Anti-forgerry token

--- Gigger/Controllers/GigsController.cs
+++ Gigger/Controllers/GigsController.cs
@@ -30,12 +30,20 @@ namespace Gigger.Controllers
 
         [Authorize]
         [HttpPost]       
+        [ValidateAntiForgeryToken]
         public ActionResult Create(GigFormViewModel viewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("Create", viewModel);
+            }
+                
+
             var gig = new Gig
             {
                 ArtistId = User.Identity.GetUserId(),
-                DateTime = viewModel.DateTime,
+                DateTime = viewModel.GetDateTime(),
                 GenreId = viewModel.Genre,
                 Venue = viewModel.Venue
 
