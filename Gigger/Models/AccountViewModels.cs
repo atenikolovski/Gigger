ee6294cc55295a770d54f74bbaf7425ace0b49a3
Atanas Nikolovski <atanas.nikolovski@students.finki.ukim.mk>
2019-08-21 14:45:52 +0200

    Added Name to the Artist

--- Gigger/Models/AccountViewModels.cs
+++ Gigger/Models/AccountViewModels.cs
@@ -68,6 +68,10 @@ namespace Gigger.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+               
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
